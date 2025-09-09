6ad6281a5179df17130a47f4b38985567e3d472f
ParthBiswas <ParthBiswas@outlook.com>
2025-09-09 19:08:47 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ import 'dotenv/config'
 
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV) {
+if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("/{*splat}", (req,res) => {
+    app.get("*", (req,res) => {
         res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"));
     });
 }
